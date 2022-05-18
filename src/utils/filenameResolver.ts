1d16281021c070d8b26d8ff8f8e44aa585b363db
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-05-18 23:01:42 +0800

    chore: change the order of filename markdown resolver

--- src/utils/filenameResolver.ts
+++ src/utils/filenameResolver.ts
@@ -4,10 +4,10 @@ export function filenameResolver(options: filenameResolverOptions) {
   const now = new Date();
   const { markdown, title, index } = options;
   return markdown
-    .replace(/<title>/g, title)
     .replace(/<index>/g,  `${index}`)
     .replace(/<year>/g, `${now.getFullYear()}`)
     .replace(/<month>/g, `${now.getMonth()+1}`)
     .replace(/<date>/g, `${now.getDate()}`)
+    .replace(/<title>/g, title)
     .replace(/\//g, '|');
 }
